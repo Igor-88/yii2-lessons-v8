74ece487da8a3dd0cfac05d5b6c1dfe3af44bf4b
IGOR-88 <urin3856@gmail.com>
2018-07-28 23:02:55 +0400

    Lesson 2. 25:08

---
+++ basic/models/Product.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Игорь-64
+ * Date: 28.07.2018
+ * Time: 22:33
+ */
+
+namespace app\models;
+
+
+use yii\base\Model;
+
+class Product extends Model
+{
+
+}
