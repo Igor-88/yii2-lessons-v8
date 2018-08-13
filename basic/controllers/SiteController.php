8179c2d9aff5f57dc9f5949c28d1d190dde33c02
IGOR-88 <urin3856@gmail.com>
2018-08-13 21:42:10 +0400

    Урок 4. Время 00:28:10.

--- basic/controllers/SiteController.php
+++ basic/controllers/SiteController.php
@@ -60,120 +60,7 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        \ob_start();
-
-/*        $query = new Query();
-        $query
-            ->select(['r.id', 'r.product_id', 'r.text'])
-            ->from(['r' => '{{reviews}}'])
-            ->where('id < 5')
-            ->indexBy(function ($row) {
-                return 'review-' . $row['id'];
-            })
-            ->join('inner join', ['p' => 'product'], 'r.product_id = p.id')*/
-            // WHERE id < 5 AND text LIKE '%was%' OR text LIKE '%have%'
-            /*->andWhere([
-                'or',
-                ['like', 'text', 'was'],
-                ['like', 'text', 'have'],
-            ])*/
-            /*->andFilterWhere([
-                'id' => 5,
-                'text' => $_GET['text'] ?? ''
-            ])
-            ->groupBy('product_id, id, text')*/
-//        ;
-
-//        var_dump(\Yii::$app->db->createCommand('SELECT 50')->queryScalar());
-
-        /*\var_dump($query->createCommand()->getRawSql());
-
-        foreach ($query->each() as $row) {
-            \var_dump($row);
-        }*/
-//        $review = $query->all();
-//
-//        \var_dump($review);
-
-        /*\Yii::$app->db->createCommand()->update('reviews', [
-            'product_id' => 9999,
-            'text' => 'foo text 07.08.18 bar',
-            ], ['id' => 11])->execute();*/
-
-        /*\Yii::$app->db->transaction(function ($db) {
-            $db->createCommand()->delete('reviews', 'id = 16')->execute();
-        });*/
-
-        /*$transaction = \Yii::$app->db->beginTransaction();
-
-        try {
-            \Yii::$app->db->createCommand()->delete('reviews', 'id = 15')->execute();
-            $transaction->commit();
-        } catch (\Exception $e) {
-            $transaction->rollBack();
-
-            $this->addLog($e);
-        }*/
-
-
-
-//        \Yii::$app->db->createCommand()->delete('reviews', 'id = 11')->execute();
-
-       /* \Yii::$app->db->createCommand()->batchInsert('reviews', [
-            'product_id', 'text',
-        ], [
-            [8888, 'foo text'],
-            [9999, 'bar text'],
-        ])->execute();*/
-
-//        $review = Reviews::find()->indexBy('id')->one();
-//        $review = new Reviews();
-//        \var_dump($review->product->name);
-
-//        $review->text = 'foo';
-//        $review->isAttributeChanged('text');
-
-//        $
-//        $review->product->name = 'fofofobar 07.08.18';
-//        $review->product->save();
-
-//        $review->save();
-
-
-        $product = new Product();
-        $product->setAttributes(
-            [
-                'name' => 'fofofofo2',
-                'count' => '200',
-                'email_provider' => '888@foo.com',
-                'provider_id' => '8888',
-                'created' => '2018-08-07',
-            ]
-        );
-
-/*        $product->save();
-
-        $reviews = new Reviews();
-        $reviews->text = 'foo text 07.08.2018';
-
-        $reviews->save();
-        $reviews->link('product', $product);*/
-
-
-        $review = Reviews::find()
-//            ->andWhere(['id' => 17])
-            ->with('product')
-            ->one();
-
-        \var_dump($review);
-
-//        $review->delete();
-
-        $content = \ob_get_clean();
-
-        return $this->render('index', [
-            'content' => $content,
-        ]);
+        return $this->render('index');
     }
 
     /**
@@ -209,13 +96,13 @@ class SiteController extends Controller
 
         return $this->goHome();
     }
-    /*
-        /**
-         * Displays contact page.
-         *
-         * @return Response|string
-         */
-    /* public function actionContact()
+
+    /**
+     * Displays contact page.
+     *
+     * @return Response|string
+     */
+     public function actionContact()
      {
          $model = new ContactForm();
          if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
@@ -233,7 +120,7 @@ class SiteController extends Controller
       *
       * @return string
       */
-    /*public function actionAbout()
+    public function actionAbout()
     {
         return $this->render('about');
     }
@@ -247,6 +134,6 @@ class SiteController extends Controller
         \var_dump($model->validate());
 
         return '';
-    }*/
+    }
 
 }
--- basic/models/User.php
+++ basic/models/User.php
@@ -3,6 +3,7 @@
 namespace app\models;
 
 use Yii;
+use yii\web\IdentityInterface;
 
 /**
  * This is the model class for table "user".
@@ -14,10 +15,16 @@ use Yii;
  * @property string $password
  * @property string $salt
  * @property string $access_token
+ * @property string $authKey
  * @property string $create_date
  */
-class User extends \yii\db\ActiveRecord
+class User extends \yii\db\ActiveRecord implements IdentityInterface
 {
+    /**
+     * Validate constant
+     */
+    const MIN_LENGTH_PASS = 5;
+
     /**
      * {@inheritdoc}
      */
@@ -25,6 +32,11 @@ class User extends \yii\db\ActiveRecord
     {
         return 'user';
     }
+
+    /*public static function find()
+    {
+        return new UserQuery(__CLASS__);
+    }*/
 
     /**
      * {@inheritdoc}
@@ -57,5 +69,123 @@ class User extends \yii\db\ActiveRecord
             'access_token' => 'Ключ авторизации',
             'create_date' => 'Дата создания',
         ];
+    }
+
+    /**
+     * Before save event handler
+     *
+     * @param bool $insert
+     *
+     * @return bool
+     */
+    public function beforeSave($insert)
+    {
+        if (parent::beforeSave($insert)) {
+            if ($this->getIsNewRecord() && !empty($this->password)) {
+                $this->salt = $this->saltGenerator();
+            }
+            if (!empty($this->password)) {
+                $this->password = $this->passWithSalt($this->password, $this->salt);
+            } else {
+                unset($this->password);
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+    /**
+     * Generate the salt
+     *
+     * @return string
+     */
+    public function saltGenerator()
+    {
+        return hash("sha512", uniqid('salt_', true));
+    }
+    /**
+     * Return pass with the salt
+     *
+     * @param $password
+     * @param $salt
+     *
+     * @return string
+     */
+    public function passWithSalt($password, $salt)
+    {
+        return hash("sha512", $password . $salt);
+    }
+    /**
+     * @inheritdoc
+     */
+    public static function findIdentity($id)
+    {
+        return static::findOne(['id' => $id]);
+    }
+    /**
+     * @inheritdoc
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return static::findOne(['access_token' => $token]);
+    }
+    /**
+     * Finds user by username
+     *
+     * @param string $username
+     *
+     * @return static|null
+     */
+    public static function findByUsername($username)
+    {
+        return static::findOne(['username' => $username]);
+    }
+    /**
+     * @inheritdoc
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * @inheritdoc
+     */
+    public function getAuthKey()
+    {
+        return $this->access_token;
+    }
+    /**
+     * @inheritdoc
+     */
+    public function validateAuthKey($authKey)
+    {
+        return $this->getAuthKey() === $authKey;
+    }
+    /**
+     * Validates password
+     *
+     * @param  string $password password to validate
+     *
+     * @return boolean if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return $this->password === $this->passWithSalt($password, $this->salt);
+    }
+    /**
+     * Generates password hash from password and sets it to the model
+     *
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $this->passWithSalt($password, $this->saltGenerator());
+    }
+    /**
+     * Generates "remember me" authentication key
+     */
+    public function generateAuthKey()
+    {
+        $this->access_token = Yii::$app->security->generateRandomString();
     }
 }
