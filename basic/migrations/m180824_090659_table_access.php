4cd43ad23d1c83a96ea235d067338d3226e5af56
IGOR-88 <urin3856@gmail.com>
2018-08-24 15:52:01 +0400

    Урок 6. Время 00:35:46. Пролистал Урок 5.

---
+++ basic/migrations/m180824_090659_table_access.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180824_090659_table_access
+ */
+class m180824_090659_table_access extends Migration
+{
+    public function up()
+    {
+        $this->execute('CREATE TABLE `access` (
+  `id` int(11) NOT NULL,
+  `note_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `since` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+ALTER TABLE `access`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_evrnt_access_1_idx` (`note_id`) USING BTREE,
+  ADD KEY `fk_evrnt_access_2_idx` (`user_id`) USING BTREE;
+
+
+ALTER TABLE `access`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;');
+    }
+
+    public function down()
+    {
+        $this->dateTime('access');
+    }
+}
