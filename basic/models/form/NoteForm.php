1b1ed0a757905a43451b562908160bdb248ffac8
IGOR-88 <urin3856@gmail.com>
2018-08-31 14:13:20 +0400

    Урок 7. Время 02:06:50.

--- basic/models/form/NoteForm.php
+++ basic/models/form/NoteForm.php
@@ -8,6 +8,7 @@
 
 namespace app\models\form;
 
+use app\models\Access;
 use app\models\Note;
 use yii\helpers\BaseArrayHelper;
 
@@ -18,12 +19,34 @@ class NoteForm extends Note
      */
     public $grantedTo = [];
 
+    public function afterFind()
+    {
+        parent::afterFind();
+
+        $accesses = Access::find()->andWhere(['note_id' => $this->id])->all();
+        foreach ($accesses as $access) {
+            $this->grantedTo[] = $access->user_id;
+        }
+    }
+
     public function rules(): array
     {
         return BaseArrayHelper::merge([
-            ['grantedTo', 'safe'],
+            ['grantedTo', 'each', 'rule' => ['integer']],
         ], parent::rules());
     }
 
+    public function afterSave($insert, $changedAttributes)
+    {
+        parent::afterSave($insert, $changedAttributes);
 
+        foreach ($this->grantedTo as $userId) {
+            $access = new Access([
+                'user_id' => $userId,
+                'note_id' => $this->id,
+            ]);
+            $access->save();
+            $this->link('access', $access);
+        }
+    }
 }
