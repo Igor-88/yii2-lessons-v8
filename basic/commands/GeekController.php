b6caa5fc9281447d2ed0cab2acbb1415ce1d64e2
IGOR-88 <urin3856@gmail.com>
2018-09-04 15:30:12 +0400

    Урок 8. Время 01:15:04.

--- basic/commands/GeekController.php
+++ basic/commands/GeekController.php
@@ -12,10 +12,11 @@ namespace app\commands;
 use app\models\Note;
 use yii\console\Controller;
 use yii\console\ExitCode;
+use yii\helpers\Console;
 
 class GeekController extends Controller
 {
-    public $mode = 'text';
+    public $mode;
 
     /**
      * @param array $ids
@@ -26,13 +27,36 @@ class GeekController extends Controller
         $notes = Note::findAll(['id' => $ids]);
         foreach ($notes as $note) {
             if ($this->mode === 'ids') {
-                echo $note->id;
+                echo $this->ansiFormat($note->id, Console::BG_GREEN, Console::FG_GREY);
             } else {
                 echo $note->text;
             }
 
             echo \PHP_EOL;
         }
+
+        return ExitCode::OK;
+    }
+
+
+    /**
+     * @return int
+     */
+    public function actionProgress(): int
+    {
+
+        $array = \array_fill(0, 9999, \random_int(0, 9999));
+        $total = count($array);
+        Console::startProgress(0, $total);
+
+        foreach ($array as $index => $value) {
+ //           \password_hash($value, PASSWORD_BCRYPT);
+            if ($index % 200 === 0) {
+                Console::updateProgress($index++, $total);
+            }
+        }
+        Console::updateProgress($total, $total);
+        Console::endProgress();
 
         return ExitCode::OK;
     }
